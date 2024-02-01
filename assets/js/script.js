9c1aaa06e462959e37fee4f870799c1b46d7e17d
Khomych1004 <khomych100488@gmail.com>
2024-02-01 03:52:28 +0000

    fetch update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,19 @@ var key = "&appid=8d876dd68a3e1244d91237d0f73f7818";
 
 var url = link + city + key;
 
-fetch(url)
+const headers = new Headers({
+    "Content-Type": "application/json",
+    "x-api-key": apiKey
+  });
+  
+  //request settings
+  var requestOptions = {
+    method: 'GET',
+    headers: headers,
+    redirect: 'follow'
+  };
+
+fetch(url, requestOptions)
 .then(function (response) {
     console.log(response.json());
     //return response.json();
