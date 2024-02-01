971000be87adc0e7bde6bd9edee4203ea5b0991f
Khomych1004 <homich8897@gmail.com>
2024-02-01 03:43:28 +0000

    changing variable name

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@ var link = "http://api.openweathermap.org/data/2.5/forecast?q=";
 var city = "London";
 var key = "&appid=8d876dd68a3e1244d91237d0f73f7818";
 
-var apiURL = link + city + key;
+var url = link + city + key;
 
-fetch(apiURL)
+fetch(url)
 .then(function (response) {
     console.log(response.json());
     //return response.json();
