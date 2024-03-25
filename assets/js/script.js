119950a88940e4617ec573d4875cea534ddfc6a2
Khomych1004 <khomych100488@gmail.com>
2024-03-25 15:16:12 +0000

    added function to display weather for five days

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // TEST CITY
 var testCity = "London";
 
-// Creates tags and fills them with data
+// Creates tags and populates them with today's data
 function creatingOneDay(data) {
   $("#today").empty();
   var city = (data.name);
@@ -22,7 +22,7 @@ function creatingOneDay(data) {
 
   // Date
   var todaysDate = $("<p>");
-  todaysDate.text("(" + date + ")");
+  todaysDate.text("( " + date + " )");
   todaysForecastDiv.append(todaysDate);
 
   // Weather Icon
@@ -41,14 +41,90 @@ function creatingOneDay(data) {
 
   // Humidity
   var todaysForecastHumidity = $("<p>");
-  todaysForecastHumidity.text("Humidity: " + humidity + "%");
+  todaysForecastHumidity.text("Humidity: " + humidity + " %");
   todaysForecastDiv.append(todaysForecastHumidity);
 
   $("#today").append(todaysForecastDiv);
 }
 
-//makes an API request and displays the received data on the screen
+// Creates tags and populates them with data in five days
+function creatingFiveDays(data) {
+  var title = $("<h3>");
+  title.addClass("title");
+  title.css({
+    "font-weight": "bold",
+    "padding-left": "0"
+  })
+  title.text("5-Day Forecast:");
+
+  $("#forecast").append(title);
+
+  for (var i = 0; i < data.list.length; i++) {
+    if (data.list[i].dt_txt.includes("12:00:00")) {
+      var date = dayjs.unix(data.list[i].dt).format("DD-MM-YYYY");
+      var icon = (data.list[i].weather[0].icon);
+      var iconLink = ("https://openweathermap.org/img/wn/" + icon + "@2x.png");
+      var temp = (data.list[i].main.temp.toFixed(2));
+      var wind = (data.list[i].wind.speed.toFixed(2));
+      var humidity = (data.list[i].main.humidity);
+
+      // 5-day Forecast Grid Layout
+      var grid = $("<div>");
+      grid.addClass("col-sm-12 col-md-12 col-lg-2");
+
+      // 5-day Forecast Cards
+      var card = $("<div>");
+      card.addClass("card");
+      grid.append(card);
+
+      // 5-day Forecast Card Body's
+      var cardBody = $("<div>");
+      cardBody.addClass("card-body");
+      cardBody.css({
+        "text-align": "left",
+        "background-color": "#303d50",
+        "color": "white",
+        "padding": "10px"
+      })
+      card.append(cardBody);
+
+      // 5-day Forecast Card Title
+      var cardTitle = $("<h6>");
+      cardTitle.addClass("card-title");
+      cardTitle.text("( " + date + " )");
+      cardBody.append(cardTitle);
+
+      // 5-day Forecast Card Icon
+      var cardIcon = $(`<img src="${iconLink}">`);
+      cardIcon.css({
+        "height": "75px",
+        "width": "75px"
+      });
+      cardBody.append(cardIcon);
+
+      // 5-day Forecast Card Temp
+      var cardTemp = $("<p>");
+      cardTemp.text("Temp: " + temp + " °C")
+      cardBody.append(cardTemp);
+
+      // 5-day Forecast Card Wind
+      var cardWind = $("<p>");
+      cardWind.text("Wind: " + wind + " km/h")
+      cardBody.append(cardWind);
+
+      // 5-day Forecast Card Humidity
+      var cardHumidity = $("<p>");
+      cardHumidity.text("Humidity: " + humidity + " %")
+      cardBody.append(cardHumidity);
+
+      $("#forecast").append(grid);
+    }
+  }
+}
+
+
+// Makes a request to the API and displays the received data for today on the screen
 getWeatherToDay(testCity)
 
-
-
+// Makes a request to the API and displays the received data for five days on the screen
+getWeatherFiveDays(testCity)
--- assets/js/weatherAPI.js
+++ assets/js/weatherAPI.js
@@ -1,12 +1,26 @@
 // API KEY
 const apiKey = "8d876dd68a3e1244d91237d0f73f7818";
-
-const link = "https://api.openweathermap.org/data/2.5/weather?q="
+// Link for today
+const linkToDay = "https://api.openweathermap.org/data/2.5/weather?q="
+// Five days link
+const linkFiveDays = "https://api.openweathermap.org/data/2.5/forecast?q="
+// Request settings
 const options = "&mode=json&units=metric&appid="
 
-function getWeatherToDay(nameCity) {
-    let queryURL = link + nameCity + options + apiKey;
+// Request data for today
+function getWeatherToDay(cityName) {
+    // Query string for today
+    const queryURL = linkToDay + cityName + options + apiKey;
 
     fetch(queryURL).then((response) => { return response.json(); })
         .then((data) => { creatingOneDay(data); });
+}
+
+// Request data for five days
+function getWeatherFiveDays(cityName) {
+    // Query line for five days
+    const queryURL = linkFiveDays + cityName + options + apiKey;
+
+    fetch(queryURL).then((response) => { return response.json(); })
+        .then((data) => { creatingFiveDays(data); });
 }
