ca1ac71d8ade9e079a8521b309baf269bf391e90
Khomych1004 <khomych100488@gmail.com>
2024-03-25 03:53:54 +0000

    code optimization

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,53 @@
-var city = "London";
-
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&mode=json&units=metric&appid=" + apiKey;
+var testCity = "London";
 
 
-fetch(queryURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data)
-    $("#today").empty();
-    var cityName = (data.name);
-    var forecastDate = dayjs.unix(data.dt).format("DD-MM-YYYY");
-    var forecastIcon = (data.weather[0].icon);
-    var forcastIconLink = ("https://openweathermap.org/img/wn/" + forecastIcon + "@2x.png");
-    var forecastTemp = (data.main.temp.toFixed(2));
-    var forecastWind = (data.wind.speed.toFixed(2));
-    var forecastHumidity = (data.main.humidity);
+function creatingOneDay(data) {
+  $("#today").empty();
+  var city = (data.name);
+  var date = dayjs.unix(data.dt).format("DD-MM-YYYY");
+  var icon = (data.weather[0].icon);
+  var iconLink = ("https://openweathermap.org/img/wn/" + icon + "@2x.png");
+  var temp = (data.main.temp.toFixed(2));
+  var wind = (data.wind.speed.toFixed(2));
+  var humidity = (data.main.humidity);
 
-    var todaysForecastDiv = $("<div>");
-    todaysForecastDiv.css({ "border": "solid black 1px", "padding": "5px", "color": "black" });
+  var todaysForecastDiv = $("<div>");
+  todaysForecastDiv.css({ "border": "solid black 1px", "padding": "5px", "color": "black" });
 
-    // City Name
-    var todaysForecastCity = $("<h2>");
-    todaysForecastCity.text(cityName);
-    todaysForecastDiv.append(todaysForecastCity);
+  // City
+  var todaysForecastCity = $("<h2>");
+  todaysForecastCity.text(city);
+  todaysForecastDiv.append(todaysForecastCity);
 
-    // Today's Date
-    var todaysDate = $("<p>");
-    todaysDate.text("(" + forecastDate + ")");
-    todaysForecastDiv.append(todaysDate);
+  // Date
+  var todaysDate = $("<p>");
+  todaysDate.text("(" + date + ")");
+  todaysForecastDiv.append(todaysDate);
 
-    // Weather Forecast Icon
-    var todaysForecastIcon = $(`<img src="${forcastIconLink}">`);
-    todaysForecastDiv.append(todaysForecastIcon);
+  // Weather Icon
+  var todaysForecastIcon = $(`<img src="${iconLink}">`);
+  todaysForecastDiv.append(todaysForecastIcon);
 
-    // Temperature Forecast
-    var todaysForecastTemp = $("<p>");
-    todaysForecastTemp.text("Temp: " + forecastTemp + " °C");
-    todaysForecastDiv.append(todaysForecastTemp);
+  // Temperature
+  var todaysForecastTemp = $("<p>");
+  todaysForecastTemp.text("Temp: " + temp + " °C");
+  todaysForecastDiv.append(todaysForecastTemp);
 
-    // Wind Forecast
-    var todaysForecastWind = $("<p>");
-    todaysForecastWind.text("Wind: " + forecastWind + " km/h");
-    todaysForecastDiv.append(todaysForecastWind);
+  // Wind
+  var todaysForecastWind = $("<p>");
+  todaysForecastWind.text("Wind: " + wind + " km/h");
+  todaysForecastDiv.append(todaysForecastWind);
 
-    // Humidity Forecast
-    var todaysForecastHumidity = $("<p>");
-    todaysForecastHumidity.text("Humidity: " + forecastHumidity + "%");
-    todaysForecastDiv.append(todaysForecastHumidity);
+  // Humidity
+  var todaysForecastHumidity = $("<p>");
+  todaysForecastHumidity.text("Humidity: " + humidity + "%");
+  todaysForecastDiv.append(todaysForecastHumidity);
 
-    $("#today").append(todaysForecastDiv);
-  });
+  $("#today").append(todaysForecastDiv);
+}
+
+
+getWeatherToDay(testCity)
+
+
+
--- assets/js/weatherAPI.js
+++ assets/js/weatherAPI.js
@@ -1,1 +1,12 @@
-var apiKey = "8d876dd68a3e1244d91237d0f73f7818";
+// API KEY
+const apiKey = "8d876dd68a3e1244d91237d0f73f7818";
+
+const link = "https://api.openweathermap.org/data/2.5/weather?q="
+const options = "&mode=json&units=metric&appid="
+
+function getWeatherToDay(nameCity) {
+    let queryURL = link + nameCity + options + apiKey;
+
+    fetch(queryURL).then((response) => { return response.json(); })
+        .then((data) => { creatingOneDay(data); });
+}
